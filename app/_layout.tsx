c048b355279005ced71a14938580b951d7bc6984
helio000 <ha2840770@gmail.com>
2025-10-15 16:29:33 -0300

    corregir os erros

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,18 +5,24 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack
+        screenOptions={{
+          headerShown: false, // remove o header de todas as telas
+        }}
+      >
+        {/* Sua tela principal (index.js) será carregada automaticamente */}
+        <Stack.Screen
+          name="modal"
+          options={{
+            presentation: 'modal',
+            title: 'Modal',
+          }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
